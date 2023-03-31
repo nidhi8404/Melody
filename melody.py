16da1696674917fdec951c1dc2888036abb7ecbe
nidhi8404 <102024307+nidhi8404@users.noreply.github.com>
2023-03-31 23:55:26 +0530

    Update melody.py

--- melody.py
+++ melody.py
@@ -1,13 +1,12 @@
 from asyncio import exceptions
 from distutils import core
-import time
-import json
+import tkinter as tk
 import spotipy
 import webbrowser
-import webbrowser
 import pyautogui as pg
+import time
+import json
 
-username = 'Nidhi Shinde'
 clientID = '39c53e21574647f29114e241ccaa7aa4'
 clientSecret = 'cfba8de7092247bea37b271672be45de'
 redirect_uri = 'http://google.com/callback/'
@@ -16,30 +15,56 @@ oauth_object = spotipy.SpotifyOAuth(clientID, clientSecret, redirect_uri)
 token_dict = oauth_object.get_access_token()
 token = token_dict['access_token']
 spotifyObject = spotipy.Spotify(auth=token)
-user_name = spotifyObject.current_user()
 
-# To print the JSON response from
-# browser in a readable format.
-# optional can be removed
-print(json.dumps(user_name, sort_keys=True, indent=4))
+class MusicPlayerGUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("Music Player")
 
-while True:
-    print("Welcome to the project, " + user_name['display_name'])
-    print("0 - Exit the console")
-    print("1 - Search for a Song")
-    user_input = int(input("Enter Your Choice: "))
-    if user_input == 1:
-        search_song = input("Enter the song name: ")
+        # Create a label to display the user's display name
+        user_name = spotifyObject.current_user()['display_name']
+        self.user_label = tk.Label(master, text="Welcome, " + user_name)
+        self.user_label.pack()
+
+        # Create a text box for the user to enter a song name
+        self.search_box_label = tk.Label(master, text="Enter a song name:")
+        self.search_box_label.pack()
+        self.search_box = tk.Entry(master)
+        self.search_box.pack()
+
+        # Create a button to search for the song
+        self.search_button = tk.Button(master, text="Search", command=self.search_song)
+        self.search_button.pack()
+
+        # Create a button to exit the program
+        self.exit_button = tk.Button(master, text="Exit", command=master.quit)
+        self.exit_button.pack()
+
+    def search_song(self):
+        # Get the user's input from the search box
+        search_song = self.search_box.get()
+
+        # Use the Spotify API to search for the song
         results = spotifyObject.search(search_song, 1, 0, "track")
         songs_dict = results['tracks']
         song_items = songs_dict['items']
-        song = song_items[0]['external_urls']['spotify']
-        webbrowser.open(song)
-        time.sleep(7) #halt
-        pg.click(311,510)
-        print('Song has opened in your browser.')
-    elif user_input == 0:
-        print("Good Bye, Have a great day!")
-        break
-    else:
-        print("Please enter valid user-input.")
+
+        if len(song_items) > 0:
+            # Get the URL of the first song in the search results
+            song_url = song_items[0]['external_urls']['spotify']
+
+            # Open the song in the user's web browser
+            webbrowser.open(song_url)
+
+            # Wait for the browser to load the page
+            time.sleep(7)
+
+            # Simulate a mouse click to play the song
+            pg.click(311,510)
+        else:
+            # Display an error message if the song is not found
+            tk.messagebox.showerror("Error", "Song not found")
+
+root = tk.Tk()
+my_gui = MusicPlayerGUI(root)
+root.mainloop()
